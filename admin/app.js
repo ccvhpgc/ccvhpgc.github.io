6f52b62add7ca909783ce6e57597237f6508d069
Arvind Kumar <thatisarvind@gmail.com>
2020-10-16 16:01:21 +0530

    Add files via upload

--- admin/app.js
+++ admin/app.js
@@ -6,25 +6,17 @@ projectId:"rexarvind",
 storageBucket:"rexarvind.appspot.com",
 messagingSenderId:"685927662051",
 appId:"1:685927662051:web:252add00d42a851bc320d6"}
-firebase.initializeApp(firebaseConfig);
-const auth=firebase.auth();
+firebase.initializeApp(firebaseConfig)
+const auth=firebase.auth()
 
 const _=id=>document.getElementById(id)
-const qsa=el=>document.querySelectorAll(el)
 
-const guestEl=qsa(".guest")
-const memberEl=qsa(".member")
+const ROOT_URL="https://rexarvind.000webhostapp.com"
 
-auth.onAuthStateChanged(user=>{
-if(user){
-memberID = user.uid;
-guestEl.forEach(el=>el.style.display="none")
-memberEl.forEach(el=>el.style.display="block")
-getProfile()
-}else{
-guestEl.forEach(el=>el.style.display="block")
-memberEl.forEach(el=>el.style.display="none")}
-})
+const guestArea=_("guestArea")
+const userArea=_("userArea")
+
+const displayEmail=_("displayEmail")
 
 const signupForm=_("signupForm")
 const signupEmail=_("signupEmail")
@@ -41,16 +33,38 @@ const resetPwdEmail=_("resetPwdEmail")
 
 const deleteAcc=_("deleteAcc")
 
-const profileForm=_("profileForm")
-const profileName=_("profileName")
-const profileClass=_("profileClass")
-const profileEmail=_("profileEmail")
-const profileWA=_("profileWA")
-const profilePhone=_("profilePhone")
-const profileErrMsg=_("profileErrMsg")
-const profileOutput=_("profileOutput")
+const updateEmailForm=_("updateEmailForm")
+const newEmail=_("newEmail")
+
+const userDetailsForm=_("userDetailsForm")
+const userFullName=_("userFullName")
+const userClass=_("userClass")
+const userEmail=_("userEmail")
+const userWhatsApp=_("userWhatsApp")
+const userPhone=_("userPhone")
+const userAbout=_("userAbout")
+const userErrMsg=_("userErrMsg")
+
+const userDetailsOutput=_("userDetailsOutput")
+
+const userImg=_("userImg")
+const userImgForm=_("userImgForm")
+const userImgFile=_("userImgFile")
+const userImgMsg=_("userImgMsg")
 
 
+
+auth.onAuthStateChanged(user=>{
+if(user){
+guestArea.style.display="none"
+userArea.style.display="block"
+userID=user.uid
+displayEmail.innerHTML=user.email
+getProfile()
+}else{
+guestArea.style.display="block"
+userArea.style.display="none"}
+})
 
 signupForm.addEventListener("submit", (e)=>{e.preventDefault()
 const email=signupEmail.value
@@ -105,22 +119,34 @@ alert("Your Account Has Been Deleted.") })
 .catch(error=>alert(error))
 })
 
+updateEmailForm.addEventListener("submit", (e)=>{e.preventDefault()
+const user=auth.currentUser;
+const newEmail=newEmailEl.value
+const credential = createCredential(user)
+changeEmail(user, credential, newEmail)
+})
+
+const changeEmail=(user, credential, newEmail)=>{
+user.reauthenticateWithCredential(credential)
+.then(()=>{user.updateEmail(newEmail)
+.then(()=>alert('Email Updated!'))
+.catch(error=>alert(error))
+}).catch(error =>alert(error))
+}
 
 
 
 
-
-
-
-profileForm.addEventListener("submit", (e)=> {e.preventDefault()
+userDetailsForm.addEventListener("submit", (e)=> {e.preventDefault()
 
 let fd=new FormData()
-fd.append("uid", memberID)
-fd.append("name", profileName.value)
-fd.append("class", profileClass.value)
-fd.append("email", profileEmail.value)
-fd.append("whatsapp", profileWA.value)
-fd.append("phone", profilePhone.value)
+fd.append("uid", userID)
+fd.append("name", userFullName.value)
+fd.append("class", userClass.value)
+fd.append("email", userEmail.value)
+fd.append("whatsapp", userWhatsApp.value)
+fd.append("phone", userPhone.value)
+fd.append("about", userAbout.value)
 
 var xhr=new XMLHttpRequest()
 
@@ -129,7 +155,10 @@ xhr.open("POST", "https://rexarvind.000webhostapp.com/api/cc/post.php" , true)
 xhr.onreadystatechange = function(){
   if(xhr.readyState==4 && xhr.status==200){
     output=JSON.parse(xhr.responseText)
-    profileErrMsg.innerHTML=output.message
+    userErrMsg.innerHTML=output.message
+    userErrMsg.style.display="block";
+    setTimeout(()=>{
+    userErrMsg.style.display="none";},3000)
     getProfile()
   }
 }
@@ -138,36 +167,66 @@ xhr.send(fd)
 
 
 
-
-
-
 function getProfile(){
-
-fetch('https://rexarvind.000webhostapp.com/api/cc/get/'+memberID)
+fetch('https://rexarvind.000webhostapp.com/api/cc/get/'+userID)
 .then(res=>res.json())
 .then(res=>showProfile(res))
-.catch(err=>console.log(err))
+.catch(err=>alert(err))
 }
 
 
 function showProfile(data){
 data=data.message
-let allData="";
+let output="";
 
-allData=`Name:<br>${data[0].name}<br><br>
-Class:<br>${data[0].class}<br><br>
-Email:<br>${data[0].email}<br><br>
-WhatsApp:<br>${data[0].whatsapp}<br><br>
-Phone:<br>${data[0].phone}<br><br>`;
+output=`<b>Name:</b> ${data[0].name}<br>
+<b>Class:</b> ${data[0].class}<br>
+<b>Email:</b> ${data[0].email}<br>
+<b>WhatsApp:</b> ${data[0].whatsapp}<br>
+<b>Phone:</b> ${data[0].phone}<br>
+<b>About:</b><span style="white-space: pre-wrap"> ${data[0].about}</span>`;
 
-profileOutput.innerHTML=allData
+userDetailsOutput.innerHTML=output
 
-profileName.value=data[0].name
-profileClass.value=data[0].class
-profileEmail.value=data[0].email
-profileWA.value=data[0].whatsapp
-profilePhone.value=data[0].phone
+userFullName.value=data[0].name
+userClass.value=data[0].class
+userEmail.value=data[0].email
+userWhatsApp.value=data[0].whatsapp
+userPhone.value=data[0].phone
+userAbout.value=data[0].about
+userImg.src=ROOT_URL+'/uploads/cc/'+userID+'-0.jpg?'+Math.random()
 }
+
+
+
+
+userImgForm.addEventListener("submit", (e)=>{
+e.preventDefault();
+userImgMsg.innerHTML="Please wait..."
+let img=userImgFile.files[0];
+
+var imgFD = new FormData();	
+imgFD.append("img", img);
+imgFD.append("uid", userID);
+
+var ajax = new XMLHttpRequest();
+ajax.open("POST", "https://rexarvind.000webhostapp.com/api/cc/photo.php");
+
+ajax.addEventListener("load", userImgComplete, false);
+ajax.onreadystatechange=function(){
+  if(ajax.readyState==4 && ajax.status==200) {
+      userImgMsg.innerHTML=ajax.responseText;
+      setTimeout(()=>{userImgMsg.innerHTML=""},3000)
+  }
+}
+ajax.send(imgFD)
+})
+
+function userImgComplete(e){
+userImg.src=ROOT_URL+'/uploads/cc/'+userID+'-0.jpg?'+Math.random()
+}
+
+
 
 
 
